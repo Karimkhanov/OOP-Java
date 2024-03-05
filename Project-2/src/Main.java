7881b7b0c1c522c468acc5b31f382be1e1a7eeec
Tursynkhan <t_karimkhanov@kbtu.kz>
2024-03-06 00:17:11 +0500

    Project-2

---
+++ Project-2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        IDandPasswords idandPasswords = new IDandPasswords();
+
+        LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+    }
+}
